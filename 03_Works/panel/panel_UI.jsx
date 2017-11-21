efa4e299ac78d070dd2130c8ab2129d04b79cb16
kobutori <kobutorina.hato@gmail.com>
2017-11-22 01:02:06 +0900

    アンドゥグループ作成

--- 03_Works/panel/panel_UI.jsx
+++ 03_Works/panel/panel_UI.jsx
@@ -8,6 +8,7 @@
     
     var a = w.add("button",[10,10,150-10,50-10],"a");
     a.onClick = function(){
+        app.beginUndoGroup('buttonA'); 
         var test = app.project.activeItem.selectedLayers[0]
 
         var pricomp = app.project.activeItem.layers.precompose([test.index],"煙","true");
@@ -21,7 +22,8 @@
         FX(1).setValue(50);
         FX(3).setValue(1);
 
-        fx01("opacity").setValue(60);    
+        fx01("opacity").setValue(60);  
+        app.endUndoGroup();
     }
     var b = w.add("button",[150,10,300-10,50-10],"b");
     
@@ -29,30 +31,31 @@
    
    
    b.onClick = function(){
+       app.beginUndoGroup('buttonB'); 
         var test = app.project.activeItem.selectedLayers[0]
-
+    
         var pricomp = app.project.activeItem.layers.precompose([test.index],"Tkou","true");
-
+        
         var thirdLayer = app.project.activeItem.selectedLayers[0].source.layer(1);
         //複製
         thirdLayer.duplicate();
         var fx02 = app.project.activeItem.selectedLayers[0].source.layer(1);
-        var FX;
-        var FX2;
+        var FX,FX2,FX3;
         //エフェクト
         FX = fx02("Effects").addProperty("ドロップシャドウ");
         FX(2).setValue(255); 
         FX(4).setValue(0);
         FX(5).setValue(100);
-/*
-        FX2 = fx02("Effects").addProperty("ドロップシャドウ2");
-        FX2(3).setValue(100);
-        FX2(7).setValue(0);
-        FX2(8).setValue(400);
-    */    
-        FX = fx02("Effects").addProperty("塗り");
-        FX(4).setValue(150);
 
+        FX2 = fx02("Effects").addProperty("ドロップシャドウ");
+        FX(2).setValue(255); 
+        FX(4).setValue(0);
+        FX(5).setValue(100);
+        
+        FX3 = fx02("Effects").addProperty("塗り");
+        FX(4).setValue(150);
+        app.endUndoGroup();
+        
     }        
         
         return w;
